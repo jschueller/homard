2551418b49d44c842102306517a770ad12b97f75
vsr <vsr@opencascade.com>
2020-04-16 10:08:19 +0300

    Copyright update 2020

--- src/tests/Test/tutorial_4.py
+++ src/tests/Test/tutorial_4.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2011-2019  CEA/DEN, EDF R&D
+# Copyright (C) 2011-2020  CEA/DEN, EDF R&D
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
